96d114b85108f27c69d2d54802e4f09689ee6476
rladmsdh <a01051355543@gmail.com>
2023-02-05 14:48:27 +0900

    도메인 수정

--- CleanArchitecture/domain/src/main/java/com/example/domain/domain/usecase/LoginUseCase.kt
+++ CleanArchitecture/domain/src/main/java/com/example/domain/domain/usecase/LoginUseCase.kt
@@ -2,8 +2,9 @@ package com.example.data.data.usecase
 
 import data.model.LoginParam
 import data.repository.LoginRepository
+import javax.inject.Inject
 
-class LoginUseCase(private val loginRepository: LoginRepository){
+class LoginUseCase @Inject constructor(private val loginRepository: LoginRepository) {
     suspend fun excute(data: LoginParam) {
         loginRepository.login(data)
     }
--- CleanArchitecture/presentation/src/main/java/presentation/di/MainApplication.kt
+++ CleanArchitecture/presentation/src/main/java/presentation/di/MainApplication.kt
@@ -1,5 +1,7 @@
 package presentation.di
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MainApplication: Application()
