822b67b7a08b98630f6c68f2a05f13b7db29f26b
rladmsdh <a01051355543@gmail.com>
2023-02-07 14:14:48 +0900

    compose 리사이클러뷰

---
+++ Recyclerview_compose/app/src/main/java/com/example/recyclerview/DataProvider.kt
@@ -0,0 +1,16 @@
+package com.example.recyclerview
+
+object DataProvider{
+    val puppyList = listOf(
+        Puppy("코코", "코코코코코 코 코코코 입!"),
+        Puppy("보리","보리 보리 쌀"),
+        Puppy("초코","초코송이 먹고싶당"),
+        Puppy("콩이", "나는 콩을 안좋아하지만 콩이는 좋아!"),
+        Puppy("사랑이", "사랑이 덕분에 집에 사랑이 가득~"),
+        Puppy("달이", "달 달 무슨 달 쟁반같이 둥근 달"),
+        Puppy("별이", "별.. 별.. 별.."),
+        Puppy("해피", "암 쏘 해피~~~~ "),
+        Puppy("마루", "마루야.. 가지마.."),
+        Puppy("까미", "난 영어 이름도 있어. Kami")
+    )
+}
---
+++ Recyclerview_compose/app/src/main/java/com/example/recyclerview/PuppyData.kt
@@ -0,0 +1,6 @@
+package com.example.recyclerview
+
+data class Puppy(
+    var name:String,
+    val content:String,
+)
