593db729d9c5c3bc798e3cd9c006f62e35c4e429
rladmsdh <a01051355543@gmail.com>
2023-02-08 15:22:17 +0900

    바텀네비게이션 구현

---
+++ Compose_practice/bottomnavigation/src/main/java/com/example/bottomnavigation/BottomNavItem.kt
@@ -0,0 +1,6 @@
+package com.example.bottomnavigation
+
+data class BottomNavItem(
+    val name : String,
+    val route : String,
+)
--- Compose_practice/bottomnavigation/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ Compose_practice/bottomnavigation/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -1,14 +1,128 @@
 package com.example.bottomnavigation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+            Scaffold(
+                bottomBar = {
+                    BottomNavigationBar(
+                        items = listOf(
+                            BottomNavItem(
+                                name = "Fist",
+                                route = "first",
+                            ),
+                            BottomNavItem(
+                                name = "Second",
+                                route = "second",
+                            )
+                        ),
+                        navController = navController,
+                        onItemClick = {
+                            navController.navigate(it.route)
+                        }
+                    )
+                }
+            ) {
+                Navigation(navController = navController)
+            }
+        }
+    }
+}
 
+@Composable
+fun Navigation(navController: NavHostController){
+    NavHost(navController = navController, startDestination = "first") {
+        composable("first"){
+            FirstScreen()
+        }
+        composable("second"){
+            SecondScreen()
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar(
+    items : List<BottomNavItem>,
+    navController: NavHostController,
+    modifier: Modifier=Modifier,
+    onItemClick:(BottomNavItem)->Unit
+){
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = Color.Gray,
+        elevation = 5.dp
+    ) {
+        //item 배열에 담긴 항목 추가
+        items.forEach{ item ->
+            // 뷰의 활동 상태를 백스에 담아 저장
+            val selected = item.route == backStackEntry.value?.destination?.route
+
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = Color.DarkGray,
+                unselectedContentColor = Color.LightGray,
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+                        Text(
+                            text = item.name,
+                            textAlign = TextAlign.Center,
+                            fontSize = 10.sp
+                        )
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun FirstScreen(){
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = { Log.d("screen","first")}) {
+            
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(){
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = { Log.d("screen","second")}) {
         }
     }
 }
