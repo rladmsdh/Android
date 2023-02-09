fa4f3c34a53ef7a7d01886cfd5c1ddf814673934
rladmsdh <a01051355543@gmail.com>
2023-02-09 15:01:36 +0900

    모듈 세팅

---
+++ CleanArchitecture/presentation_compose/src/main/java/com/example/presentation_compose/view/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.presentation_compose.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.presentation_compose.ui.theme.CleanArchitectureTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Login()
+        }
+    }
+}
+
+@Composable
+fun Login() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview() {
+    Login()
+}
