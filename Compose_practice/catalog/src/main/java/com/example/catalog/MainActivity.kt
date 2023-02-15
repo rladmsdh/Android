490e8fa12631e0c66680069a1d258aa79425e9fc
rladmsdh <a01051355543@gmail.com>
2023-02-15 15:45:03 +0900

    카탈로그

---
+++ Compose_practice/catalog/src/main/java/com/example/catalog/MainActivity.kt
@@ -0,0 +1,146 @@
+package com.example.catalog
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.catalog.ui.theme.ComposePracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePracticeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CatalogExample(items)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Item(itemData: ItemData) {
+    Card(
+        elevation = 8.dp,
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Image(
+                painter = painterResource(id = itemData.imageId),
+                contentDescription = itemData.title
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = itemData.title,
+                style = MaterialTheme.typography.h4
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = itemData.description,
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ItemPreview() {
+    ComposePracticeTheme {
+        Item(
+            ItemData(
+                imageId = R.drawable.ic_launcher_background,
+                title = "해변 놀이 공원",
+                description = "해변 놀이 공원 설명입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+            )
+        )
+    }
+}
+
+@Composable
+fun CatalogExample(itemList: List<ItemData>) {
+    LazyColumn {
+        items(itemList) { item ->
+            Item(itemData = item)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposePracticeTheme {
+        CatalogExample(items)
+    }
+}
+
+data class ItemData(
+    @DrawableRes val imageId: Int,
+    val title: String,
+    val description: String,
+)
+
+val items = listOf(
+    ItemData(
+        imageId = R.drawable.ic_launcher_background,
+        title = "해변 놀이 공원",
+        description = "해변 놀이 공원 설명입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.ic_launcher_background,
+        title = "캐년",
+        description = "미국의 캐년입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.ic_launcher_background,
+        title = "워터월드",
+        description = "워터월드입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.ic_launcher_background,
+        title = "미국의 캐년",
+        description = "미국의 캐년입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.ic_launcher_background,
+        title = "라스베가스",
+        description = "라스베가스입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.ic_launcher_background,
+        title = "호르슈 밴드",
+        description = "호르슈 밴드입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.ic_launcher_background,
+        title = "미국의 길",
+        description = "미국의 길입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.ic_launcher_background,
+        title = "엔텔로프 캐년",
+        description = "엔텔로프 캐년입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.ic_launcher_background,
+        title = "그랜드 캐년",
+        description = "그랜드 캐년입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+)
