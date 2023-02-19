fbbdcb1d5e7524c5b9da8b64302e9aa70c0d8b9e
rladmsdh <a01051355543@gmail.com>
2023-02-19 21:29:42 +0900

    slot api 연습

--- Compose_practice/textfield/src/main/java/com/example/textfield/MainActivity.kt
+++ Compose_practice/textfield/src/main/java/com/example/textfield/MainActivity.kt
@@ -5,12 +5,10 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -24,18 +22,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TextFieldScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        var text = remember { mutableStateOf("") }
+
+    val text1 = remember { mutableStateOf("") }
+    val text2 = remember { mutableStateOf("") }
+
+    Column {
+        TextFieldWithSlot(
+            text = text1,
+            label = { Text(text = "에딧텍스트1") },
+            clicked = { Log.d("text1",text1.value) }
+        )
+        TextFieldWithSlot(
+            text = text2,
+            label = { Text(text = "에딧텍스트2") },
+            clicked = { Log.d("text2",text2.value) }
+        )
+    }
+}
+@Composable
+fun TextFieldWithSlot(
+    text: MutableState<String>,
+    label: @Composable () -> Unit,
+    clicked: () -> Unit,
+) {
+    Column {
         OutlinedTextField(
             value = text.value,
             onValueChange = {fieldValue -> text.value = fieldValue },
-            label ={ Text("에딧텍스트") }
+            label = label
         )
-        Button(onClick = { Log.d("text",text.value) }) {
+        Button(onClick = clicked) {
             Text(text = "버튼")
         }
     }
 }
+
 
 @Composable
 @Preview(showBackground = true)
